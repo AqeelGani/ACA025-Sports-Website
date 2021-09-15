77b6fd4c6230a70c27ca13ab662ddb989dc7b948
AqeelGani <88658847+AqeelGani@users.noreply.github.com>
2021-09-15 08:08:00 +0530

    Add files via upload

---
+++ scriptiplresults.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAlJnxnNayuW08Ek0W0FiueBqp_qwP8-JM",
+    authDomain: "aca025-sports-website.firebaseapp.com",
+    projectId: "aca025-sports-website",
+    storageBucket: "aca025-sports-website.appspot.com",
+    messagingSenderId: "162884738688",
+    appId: "1:162884738688:web:84f00b319ae6257bd55065"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
